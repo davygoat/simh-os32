fe6ef7c142204d8968248b74d9fa96d4b932e299
David Wijnants <david@slackware.fred.com>
2020-02-16 14:00:05 +0000

    Use for loop for stages.

--- rebuild-system.sh
+++ rebuild-system.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-
-./id32 stage-00-prelim.ini    || exit
-./id32 stage-01-os32base.ini  || exit
-./id32 stage-02-sysgen.ini    || exit
-./id32 stage-03-mtm.ini       || exit
-./id32 stage-04-compilers.ini || exit
-./id32 stage-05-scripts.ini   || exit
-./id32 stage-06-examples.ini  || exit
-./id32 stage-99-done.ini      || exit
-
+for stage in stage-*.ini; do
+   ./id32 $stage || exit
+done
 exit
