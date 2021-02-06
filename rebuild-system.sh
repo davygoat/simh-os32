fb42549c1a4145c7e3130baa75bc341c71587a67
David Wijnants <david@slackware.fred.com>
2021-02-06 00:14:40 +0000

    Automate "next steps".
    - Launch xterm.
    
    - Establish expect/send dialog on PAS line 0 to do our first signon.
    
    - USERINIT.CSS then does the WILD and SEARCH compile *before* EOUINIT.
    
    - When we get to EOUINIT, do a SHUTDOWN.
    
    - And we're done. No manual interaction required.

--- rebuild-system.sh
+++ rebuild-system.sh
@@ -2,31 +2,4 @@
 for stage in stage-*.ini; do
    ./id32 $stage || exit
 done
-cat <<EOD
-
-
-Next steps:
-
-- Open up two more terminals.
-
-  - Terminal 0:   leave this terminal visible
-  - Terminal 1:   ./id32 os32.ini    or    ./OS32-FTPd
-  - Terminal 2:   telnet localhost 1026
-
-- Type these commands in terminal 2 (telnet session):
-
-  SIGNON whatever,25,user1
-      ... wait for 'wild' and 'search' programs to be compiled ...
-  SIGNOFF
-
-  Hit Ctrl+] and quit
-
-- Type this command in terminal 1 (system console, SimH/id32 window):
-
-  SHUTDOWN
-
-- Done
-
-
-EOD
 exit
