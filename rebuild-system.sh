3ec0e123d6f73a313b88faafaa6117c862b6f9a9
David Wijnants <david@slackware.fred.com>
2021-03-18 16:55:35 +0000

    kill telnet before shutdown, so we don't have to TIME_WAIT afterwards.

--- rebuild-system.sh
+++ rebuild-system.sh
@@ -2,16 +2,3 @@
 for stage in stage-*.ini; do
    ./id32 $stage || exit
 done
-echo
-echo Waiting for TIME_WAIT socket to go away.
-echo
-echo This can take up to 90 seconds...
-echo
-for ((cc=90; cc>=0; cc--)); do
-   printf "\r   $cc "
-   sleep 1s
-done
-printf "\rBegone!       \n"
-echo
-echo
-exit
