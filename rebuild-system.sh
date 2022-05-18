83e65457b497e36300c0c3fc3ba1de9f7c415b3c
David Wijnants <david@slackware.fred.com>
2022-05-18 19:49:23 +0100

    Cleanup and SimH V3 check (build scripts only work with V4).

--- rebuild-system.sh
+++ rebuild-system.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+echo
+echo
+if [ -n "`echo exit | ./id32 | tee /dev/tty | grep 'Interdata 32b simulator V3'`" ]; then
+   echo
+   echo
+   echo
+   echo "    *** THE BUILD SCRIPTS ONLY WORK WITH SIMH V4 (GITHUB) ***"
+   echo
+   echo
+   echo
+   exit 1
+fi
 mkdir -p logs
 rm -f logs/*
 for stage in stage-*.ini; do
